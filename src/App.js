12881a3639be44ad9e9c33bdf88ae3570e54f933
parkjw1104 <parkjw1104@naver.com>
2022-04-19 17:24:12 +0900

    4장 04-2 까지 완성된 소스코드

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+function Alcohol({name,picture}){
+  return (
+    <div>
+  <h2>I like {name}</h2>
+  <img src={picture}/>
+  </div>
+  );
+
+}
+
+const alcoholILike = [
+  {
+    name: 'Soju',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.i0xxvU_laI9FQfixe6lKggHaHL%26pid%3DApi&f=1'
+  },
+  {
+    name: 'Bear',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.s5KoCoaK8V2SjCp7Sfpm1AHaH5%26pid%3DApi&f=1'
+  },
+  {
+    name: 'Makgeolli',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.CMURZPZSpJzd7Na_9tVvTQHaFj%26pid%3DApi&f=1'
+  },
+  {
+    name: 'Vodka',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.yfCj_Bm_Arva5mkKE7G-8QHaLb%26pid%3DApi&f=1'
+  },
+  {
+    name: 'cocktail',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JMDVbaUxX_gsZZy7vIpCEgHaHa%26pid%3DApi&f=1'
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     {alcoholILike.map(glass => <Alcohol key= {glass.name} name={glass.name} picture = {glass.image}/>)}
     </div>
   );
 }
